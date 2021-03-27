4e2d0e4ed750482d453944a9c92dbf564a82f8b2
Rúben ferreira <56252824+Diamanterub@users.noreply.github.com>
2021-03-27 09:06:53 +0000

    Ships done

--- Assets/Scripts/SpawnInvaders.cs
+++ Assets/Scripts/SpawnInvaders.cs
@@ -10,6 +10,9 @@ public class SpawnInvaders : MonoBehaviour
 
     [SerializeField]
     GameObject invasorB;
+    
+    [SerializeField]
+    GameObject invasorC;
 
     [SerializeField]
     int nInvadores = 7;
@@ -18,17 +21,50 @@ public class SpawnInvaders : MonoBehaviour
     float xMin = -3f;
 
 
-    void awake()
+    private void Awake()
     {
         /*
-         * Pegar num invader, e coloca nesta posição
-         * Repete
+         * Gets an invader from prefab and puts in that position
         */
         float x = xMin;
-        for(int i = 1; i <= nInvadores; i++)
+        for (int i = 1; i <= nInvadores; i++)
         {
-            GameObject newInvader = Instantiate(invasorA, transform);   
-            newInvader.transform.position = new Vector3(x, 0.5f, 0f);
+            //First row of invaders bot to top
+            //Invader A
+            GameObject newInvader = Instantiate(invasorA, transform);
+            newInvader.transform.position = new Vector3(x, 0f, 0f);
+
+            //Second row of invaders bot to top
+            //Invader A
+            for (int j = 1; j <= nInvadores; j++)
+            {
+                newInvader = Instantiate(invasorA, transform);
+                newInvader.transform.position = new Vector3(x, 0.5f, 0f);
+            }
+
+            //Trird row of invaders bot to top
+            //Invader B
+            for (int k = 1; k <= nInvadores; k++)
+            {
+                newInvader = Instantiate(invasorB, transform);
+                newInvader.transform.position = new Vector3(x, 1f, 0f);
+            }
+
+            //Forth row of invaders bot to top
+            //Invader B
+            for (int l = 1; l <= nInvadores; l++)
+            {
+                newInvader = Instantiate(invasorB, transform);
+                newInvader.transform.position = new Vector3(x, 1.5f, 0f);
+            }
+
+            //Fifth row of invaders bot to top
+            //Invader C
+            for (int p = 1; p <= nInvadores; p++)
+            {
+                newInvader = Instantiate(invasorC, transform);
+                newInvader.transform.position = new Vector3(x, 2f, 0f);
+            }
             x += 1f;
         }
     }
