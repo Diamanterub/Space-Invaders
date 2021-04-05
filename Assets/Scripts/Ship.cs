513dedd23acaa71c228d9ab47523c407ddbe2fd3
Rúben ferreira <56252824+Diamanterub@users.noreply.github.com>
2021-04-05 17:03:54 +0100

    Done

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [SerializeField] GameObject fire;
+    [SerializeField] Transform nozzle;
+    [SerializeField] float velocidade = 5f;
+    [SerializeField] int lives = 3;
+    float minX, maxX;
+    void Start()
+    {
+        // 0.25 é metade da metade do tamanho da nave
+        minX = Camera.main.ViewportToWorldPoint(Vector2.zero).x + 0.25f;
+        maxX = Camera.main.ViewportToWorldPoint(Vector2.one).x - 0.25f;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(fire, nozzle.position, nozzle.rotation);
+        }
+        MoveShip();
+    }
+    void MoveShip()
+    {
+        float hMov = Input.GetAxis("Horizontal");
+        transform.position += hMov * velocidade * Vector3.right * Time.deltaTime;
+
+        Vector3 position = transform.position;
+        position.x = Mathf.Clamp(position.x, minX, maxX);
+        transform.position = position;
+    }
+    //Vidas para a nave
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+        lives--;
+        Debug.Log(lives);
+        //Caso vidas sejam 0 o gameobject Desaparece
+        if (lives == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/SpawnInvaders.cs
+++ Assets/Scripts/SpawnInvaders.cs
@@ -4,68 +4,35 @@ using UnityEngine;
 
 public class SpawnInvaders : MonoBehaviour
 {
-
-    [SerializeField]
-    GameObject invasorA;
-
-    [SerializeField]
-    GameObject invasorB;
-    
-    [SerializeField]
-    GameObject invasorC;
-
-    [SerializeField]
-    int nInvadores = 7;
-
-    [SerializeField]
-    float xMin = -3f;
-
-
+    [SerializeField] GameObject[] Invaders;
+    [SerializeField] GameObject[] InvadersInd;
+    [SerializeField] int n_Invaders = 7; // Por linha
+    [SerializeField] float minX = -3f;
+    [SerializeField] float minY = -0.5f;
+    [SerializeField] float xInc = 1f;
+    [SerializeField] float yInc = 0.5f;
+    [SerializeField] float probInd = 0.15f;
     private void Awake()
     {
-        /*
-         * Gets an invader from prefab and puts in that position
-        */
-        float x = xMin;
-        for (int i = 1; i <= nInvadores; i++)
+        float y = minY;
+        for (int i = 0; i < Invaders.Length; i++) // Número de linhas ocupadas pelos invaders
         {
-            //First row of invaders bot to top
-            //Invader A
-            GameObject newInvader = Instantiate(invasorA, transform);
-            newInvader.transform.position = new Vector3(x, 0f, 0f);
-
-            //Second row of invaders bot to top
-            //Invader A
-            for (int j = 1; j <= nInvadores; j++)
+            for (int j = 0; j < n_Invaders; j++) // Número de invaders por linha
             {
-                newInvader = Instantiate(invasorA, transform);
-                newInvader.transform.position = new Vector3(x, 0.5f, 0f);
+                GameObject normalOuIndestrutivel;
+                if(Random.value < probInd)
+                {
+                    normalOuIndestrutivel = InvadersInd[i];
+                } 
+                else
+                {
+                    normalOuIndestrutivel = Invaders[i];
+                }
+                GameObject newInvader = Instantiate(normalOuIndestrutivel, transform);
+                newInvader.transform.position = new Vector3(minX + j * xInc, y, 1f);
             }
-
-            //Trird row of invaders bot to top
-            //Invader B
-            for (int k = 1; k <= nInvadores; k++)
-            {
-                newInvader = Instantiate(invasorB, transform);
-                newInvader.transform.position = new Vector3(x, 1f, 0f);
-            }
-
-            //Forth row of invaders bot to top
-            //Invader B
-            for (int l = 1; l <= nInvadores; l++)
-            {
-                newInvader = Instantiate(invasorB, transform);
-                newInvader.transform.position = new Vector3(x, 1.5f, 0f);
-            }
-
-            //Fifth row of invaders bot to top
-            //Invader C
-            for (int p = 1; p <= nInvadores; p++)
-            {
-                newInvader = Instantiate(invasorC, transform);
-                newInvader.transform.position = new Vector3(x, 2f, 0f);
-            }
-            x += 1f;
+            y += yInc; // Incrementação a cada nova linha
         }
     }
 }
+
